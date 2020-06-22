659d356ba15a82510cc1c510420cd79c7215d6a8
Joshua Mora <jshm101@gmail.com>
2020-06-22 09:13:53 -0700

    feat: basic currency line chart implementation
    Implements a line chart for cryptocurrency price data using victory data
    visualization library

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,81 @@
+import React, { useState } from 'react'
 import moment from 'moment'
+
+import {
+  VictoryChart,
+  VictoryLine,
+  VictoryAxis
+} from 'victory'
 
 import {
   COIN_MARKET_CAP_API_BASE_URL,
   COIN_MARKET_CAP_AUTH_HEADER,
   COIN_MARKET_CAP_API_KEY,
   NOMICS_API_BASE_URL,
-  NOMICS_API_KEY
+  NOMICS_API_KEY,
+  VICTORY_LINE_CHART_THEME
 } from '../constants'
 
 function Home({ currencies, chartData }) {
-  console.log('currencies: ', currencies)
-  console.log('chartData: ', chartData)
+  const [selectedCurrency, setSelectedCurrency] = useState(currencies[0])
 
+  const generateChartInput = () => {
+    const selectedCurrencyChartData = chartData.find(chartDataEntry =>
+      chartDataEntry.currency === selectedCurrency.symbol
+    )
+
+    const { prices, timestamps } = selectedCurrencyChartData
+
+    const chartInput = prices.map((price, index) => {
+      const date = timestamps[index]
+      return {
+        date,
+        price: Number(price)
+      }
+    })
+
+    return chartInput
+  }
+
+  const getXAxisLabel = date => (
+    moment(date).format('MMM YY')
+  )
+
+  const data = generateChartInput()
   return (
-    <div>
-      <h2>hello, world</h2>
+    <div style={{ maxWidth: 768 }}>
+      <VictoryChart
+        width={768}
+        height={500}
+        theme={VICTORY_LINE_CHART_THEME}
+        domainPadding={{ y: 50 }}
+        padding={100}
+      >
+        <VictoryLine
+          data={data}
+          interpolation="natural"
+          x="date"
+          y="price"
+          animate={{
+            duration: 2000,
+            onLoad: { duration: 750 }
+          }}
+        />
+        <VictoryAxis
+          tickValues={data.map(dataPoint => dataPoint.date)}
+          tickFormat={getXAxisLabel}
+          tickCount={6}
+        />
+        <VictoryAxis dependentAxis />
+      </VictoryChart>
+
+      <style jsx global>
+        {`
+          html {
+            background: #171717;
+          }
+        `}
+      </style>
     </div>
   )
 }
